ad48dc5cb338c7c3748a3b34b9454713486ad8de
shuuu109 <2648659985@qq.com>
2026-09-14 15:10:00 +0800

    MiniMind Decoder Bloc

--- model/model.py
+++ model/model.py
@@ -202,3 +202,27 @@ class FeedForward(nn.Module):
 
     def forward(self, x):
         return self.down_proj(self.act_fn(self.gate_proj(x)) * self.up_proj(x)) 
+
+# MiniMind Decoder Block
+class MiniMindBlock(nn.Module):
+    def __init__(self, layerid: int, config: MiniMindConfig):
+        super().__init__()
+        self.self_attn = Attention(config)
+        self.input_layernorm = RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+        self.post_attention_layernorm = RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+        self.mlp = FeedForward(config) if not config.use_moe else MOEFeedForward(config)
+
+    def forward(self, hidden_states, position_embeddings, past_key_value=None, use_cache=False, attention_mask=None):
+        # 1. Self-Attention
+        residual = hidden_states
+        hidden_states, present_key_value = self.self_attn(
+            self.input_layernorm(hidden_states),
+            position_embeddings,
+            past_key_value,
+            use_cache,
+            attention_mask
+        )
+        hidden_stastes += residual
+        hidden_states = hidden_states + self.mlp(self.post_attention_layernorm(hidden_states))
+        return hidden_states, present_key_value    
+
